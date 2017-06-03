aaf93f0794c6c137e017e7c2eff0aaa29a85a453
jemison <jemison321@gmail.com>
2017-06-03 20:28:01 -0300

    teste deploy a2

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,45 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 # Default value for keep_releases is 5
 set :keep_releases, 5
 
-after 'deploy:publishing', 'deploy:restart'
+after 'deploy:publishing', 'unicorn:restart'
+
+namespace :unicorn do
+    desc 'Stop Unicorn'
+    task :stop do
+            on roles(:app) do
+            if test("[ -f #{fetch(:unicorn_pid)} ]")
+                execute :kill, capture(:cat, fetch(:unicorn_pid))
+            end
+        end
+    end
+
+    desc 'Start Unicorn'
+    task :start do
+        on roles(:app) do
+            within current_path do
+                with rails_env: fetch(:rails_env) do
+                execute :bundle, "exec unicorn -c #{fetch(:unicorn_config)} -D"
+                end
+            end
+        end
+    end
+
+    desc 'Reload Unicorn without killing master process'
+    task :reload do
+        on roles(:app) do
+            if test("[ -f #{fetch(:unicorn_pid)} ]")
+                execute :kill, '-s USR2', capture(:cat, fetch(:unicorn_pid))
+            else
+                error 'Unicorn process not running'
+            end
+        end
+    end
+
+    desc 'Restart Unicorn'
+    task :restart
+    before :restart, :stop
+    before :restart, :start
+end
 
 namespace :deploy do
   task :restart do
@@ -44,43 +82,7 @@ namespace :deploy do
   end
 
 
-  namespace :unicorn do
-    desc 'Stop Unicorn'
-    task :stop do
-      on roles(:app) do
-        if test("[ -f #{fetch(:unicorn_pid)} ]")
-          execute :kill, capture(:cat, fetch(:unicorn_pid))
-        end
-      end
-    end
 
-    desc 'Start Unicorn'
-    task :start do
-      on roles(:app) do
-        within current_path do
-          with rails_env: fetch(:rails_env) do
-            execute :bundle, "exec unicorn -c #{fetch(:unicorn_config)} -D"
-          end
-        end
-      end
-    end
-
-    desc 'Reload Unicorn without killing master process'
-    task :reload do
-      on roles(:app) do
-        if test("[ -f #{fetch(:unicorn_pid)} ]")
-          execute :kill, '-s USR2', capture(:cat, fetch(:unicorn_pid))
-        else
-          error 'Unicorn process not running'
-        end
-      end
-    end
-
-    desc 'Restart Unicorn'
-    task :restart
-    before :restart, :stop
-    before :restart, :start
-  end
 
 
 
