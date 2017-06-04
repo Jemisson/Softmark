724bdff3de871974e7d270ca623f14ad33394e50
jemison <jemison321@gmail.com>
2017-06-04 01:48:15 -0300

    Teste Final se Deus quiser agora vai

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 # Default value for keep_releases is 5
 set :keep_releases, 5
 
-befor 'deploy:starting', 'unicorn:stop'
+before 'deploy:starting', 'unicorn:stop'
 after 'deploy:publishing', 'unicorn:start'
 
 namespace :unicorn do
