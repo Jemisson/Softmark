e66432d34f09aaff2e4ef5138a8294f37f9428aa
jemison <jemison321@gmail.com>
2017-06-05 01:28:03 -0300

    Ajustes caminho imagem anuncio 3

--- app/models/advertising.rb
+++ app/models/advertising.rb
@@ -7,15 +7,12 @@ class Advertising < ActiveRecord::Base
   validates_presence_of :picture
 
   has_attached_file :picture,
-                              :url => '/system/:attachment/:id/:style/:filename',
-                              :path => ':rails_root/public/system/:attachment/:id/:style/:filename',
-                              :storage => :filesystem,
-                              styles: { large:"1920x1080#", thumb: "100x100>" },
-                                      default_url: "/images/:style/missing.png"
+                    :url => '/system/:attachment/:id/:style/:filename',
+                    :path => ':rails_root/public/system/:attachment/:id/:style/:filename',
+                    :storage => :filesystem,
+                    styles: { large:"1920x1080#", thumb: "100x100>" },
+                    default_url: "/images/:style/missing.png"
 
 
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
-
-
-
 end
